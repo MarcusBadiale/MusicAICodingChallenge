d37418da65f24078f6b478b0e2b9d1a40bb403b1
Marcus Badiale <marcus.badiale@gmail.com>
2026-05-20 11:15:04 -0300

    first commit

---
+++ MusicAICodingChallenge/MusicAICodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAICodingChallengeApp.swift
+//  MusicAICodingChallenge
+//
+//  Created by Marcus Badiale on 20/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicAICodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicAICodingChallengeTests/MusicAICodingChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAICodingChallengeTests.swift
+//  MusicAICodingChallengeTests
+//
+//  Created by Marcus Badiale on 20/05/26.
+//
+
+import Testing
+@testable import MusicAICodingChallenge
+
+struct MusicAICodingChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
